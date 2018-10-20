8c29598445e12614d04708408e785058a42002ba
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-10-20 17:04:04 -0500

    Add basic effects code

---
+++ Maze_Shooter/Assets/Scripts/Effects/EffectsBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class EffectsBase : MonoBehaviour
+{
+	[AssetsOnly, AssetList(Path = "Prefabs/Effects/")]
+	public GameObject effectPrefab;
+
+	[Tooltip("How long the instantiated effect will last until it's destroyed")]
+	public float lifetime = 5;
+
+	static GameObject _effectsParent;
+	static GameObject _newInstance;
+
+	protected virtual void InstantiateEffect()
+	{
+		if (effectPrefab == null)
+		{
+			Debug.LogWarning("Effect prefab is not set for " + name, gameObject);
+			return;
+		}
+
+		if (_effectsParent == null)
+			_effectsParent = new GameObject("Effects");
+		
+		Destroy(Instantiate(
+			effectPrefab, transform.position, transform.rotation, _effectsParent.transform), lifetime);
+	}
+}
