d7b05a8178bf3d89a948f878900b6dc25fb483e0
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-10-24 10:06:44 -0500

    Add 'game master' script and object

---
+++ Maze_Shooter/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameMaster : ScriptableObject
+{
+    public float playerRespawnTime = 3;
+    
+    public void BeginGame() 
+    {
+
+    }
+
+    public void SpawnAnotherPlayer()
+    {
+        
+    }
+}
