4d3131e0ad50df7cc463e84ef45c07c72911df5b
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-11-06 20:15:42 -0600

    Add Camera Zone

---
+++ Maze_Shooter/Assets/Scripts/CameraZone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Arachnid;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraZone : MonoBehaviour
+{
+	public CinemachineVirtualCamera vCam;
+	[Tooltip("An object that belongs to any of these collections will trigger this")]
+	public List<Collection> triggeringCollections;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		CollectionElement element = other.GetComponent<CollectionElement>();
+		if (!element) return;
+		
+		if (!triggeringCollections.Contains(element.collection)) return;
+		EnterAction(other);
+	}
+
+
+	void EnterAction(Collider2D other)
+	{
+		vCam.Follow = other.transform;
+	}
+
+}
