d63fea78ebf24a34959e03e0159440c85d65bc20
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-11-10 16:40:28 -0600

    Add sprite sorting helper component

---
+++ Maze_Shooter/Assets/Scripts/TakeSortingOfParent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer)), ExecuteInEditMode]
+public class TakeSortingOfParent : MonoBehaviour
+{
+	public int offset;
+	SpriteRenderer _parentSpriteRenderer;
+	SpriteRenderer _spriteRenderer;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+		_parentSpriteRenderer = GetComponentInParent<SpriteRenderer>();
+		Execute();
+	}
+
+	void Execute()
+	{
+		if (!_spriteRenderer || !_parentSpriteRenderer) return;
+
+		_spriteRenderer.sortingLayerID = _parentSpriteRenderer.sortingLayerID;
+		_spriteRenderer.sortingOrder = _parentSpriteRenderer.sortingOrder + offset;
+	}
+}
