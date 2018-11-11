0bc1a81c1fa53efcb1713961507ae9bb7e07cccf
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-11-10 19:21:27 -0600

    Add coroutine helper - call coroutines from scriptable objects!

---
+++ Maze_Shooter/Assets/Arachnid/CoroutineHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arachnid
+{
+    public class CoroutineHelper : MonoBehaviour
+    {
+        static CoroutineHelper _coroutineObject;
+
+        public static void NewCoroutine(IEnumerator coroutine)
+        {
+            if (_coroutineObject == null)
+            {
+                GameObject coroutineHelper = new GameObject("Coroutine Helper");
+                _coroutineObject = coroutineHelper.AddComponent<CoroutineHelper>();
+            }
+
+            _coroutineObject.StartCoroutine(coroutine);
+        }
+    }
+}
