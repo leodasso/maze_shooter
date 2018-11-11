449933bc3995346b8d134a3141a823fbf8c518f5
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-11-10 19:21:38 -0600

    Add time warp scriptable objects

---
+++ Maze_Shooter/Assets/Scripts/TimeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu]
+public class TimeController : ScriptableObject
+{
+    public AnimationCurve timeBendCurve;
+    public float duration = 1;
+
+    [Button]
+    public void DoTimeBend()
+    {
+        if (!Application.isPlaying)
+        {
+            Debug.LogWarning("This only works in play mode!");
+            return;
+        }
+        Arachnid.CoroutineHelper.NewCoroutine(BendTime());
+    }
+
+    IEnumerator BendTime()
+    {
+        float elapsed = 0;
+
+        while (elapsed < duration)
+        {
+            Time.timeScale = timeBendCurve.Evaluate(elapsed);
+            elapsed += Time.unscaledDeltaTime;
+            yield return null;
+        }
+        Time.timeScale = 1;
+    }
+}
