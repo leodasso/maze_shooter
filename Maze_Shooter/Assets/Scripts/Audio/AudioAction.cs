6c8edeeecab5b9e998fdea42bf5523ec8af2b0d5
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-11-10 18:10:09 -0600

    Add audio scripts, mixers, and some collections

---
+++ Maze_Shooter/Assets/Scripts/Audio/AudioAction.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class AudioAction : MonoBehaviour
+{
+	[AssetsOnly]
+	public AudioCollection audioCollection;
+
+	[ToggleLeft]
+	public bool playOnAwake = false;
+
+	static GameObject _audioParent;
+
+	static GameObject AudioParent()
+	{
+		if (_audioParent) return _audioParent;
+		_audioParent = new GameObject("Audio");
+		return _audioParent;
+	}	
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (playOnAwake) Play();
+	}
+
+	[Button]
+	public void Play()
+	{
+		if (audioCollection == null)
+		{
+			Debug.LogWarning(name + " has no audio connection referenced!");
+			return;
+		}
+		
+		GameObject audioGO = new GameObject(audioCollection.name);
+		audioGO.transform.parent = AudioParent().transform;
+		AudioSource newSource = audioGO.AddComponent<AudioSource>();
+		newSource.clip = audioCollection.GetRandomClip();
+		newSource.playOnAwake = false;
+		newSource.outputAudioMixerGroup = audioCollection.mixerGroup;
+		newSource.pitch = audioCollection.Pitch();
+		newSource.Play();
+		Destroy(audioGO, audioCollection.audioLifetime);
+	}
+}
