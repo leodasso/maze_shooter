f9bec11581865137f0bb1ecde3e41aa19038efdd
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-11-12 17:48:28 -0600

    Add more curve functionality

--- Maze_Shooter/Assets/Arachnid/CurveObject.cs
+++ Maze_Shooter/Assets/Arachnid/CurveObject.cs
@@ -11,12 +11,28 @@ namespace Arachnid
 		public float multiplier = 1;
 		public AnimationCurve curve;
 
+		public enum CurveMode
+		{
+			Clamp,
+			Loop
+		}
+
+		[Tooltip("When retrieving the output (y-axis) for an input (x-axis), should it assume the curve loops or clamps?")]
+		public CurveMode curveMode = CurveMode.Clamp;
+
 		[MultiLineProperty()]
 		public string description;
 
 		public float ValueFor(float xAxisInput)
 		{
-			return curve.Evaluate(xAxisInput) * multiplier;
+			float processedX = xAxisInput;
+			if (curveMode == CurveMode.Loop)
+			{
+				float remainder = xAxisInput % curve.Duration();
+				processedX = curve.StartTime() + remainder;
+			}
+			
+			return curve.Evaluate(processedX) * multiplier;
 		}
 	}
 }
