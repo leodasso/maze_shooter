6077db3daa7c0e57db0f6a5fc55432ff5621f311
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-11-14 00:09:48 -0600

    Add transition framework (and default transition)
    What I mean by transition is like, a fade out/fade in to cover up scene loading

---
+++ Maze_Shooter/Assets/Scripts/Transition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+	public float duration = .5f;
+
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Called by animation
+	public void DestroyMe()
+	{
+		Destroy(gameObject);
+	}
+
+	public void InstantiateMe()
+	{
+		Instantiate(gameObject);
+	}
+}
