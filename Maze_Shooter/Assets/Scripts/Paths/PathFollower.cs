e9fd2221b8a9f7804f4bc7a3912165ded3491a77
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-11-16 22:51:13 -0600

    Add simple pathway functionality (WIP)

---
+++ Maze_Shooter/Assets/Scripts/Paths/PathFollower.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace Paths
+{
+	[ExecuteInEditMode]
+	public class PathFollower : MonoBehaviour
+	{
+		public Pathway pathway;
+		[Range(0, 1)]
+		public float progress;
+		public Vector2 offset;
+		[ToggleLeft, Toggle("Adopt the sorting order of the pathnodes as it traverses the path")]
+		public bool controlSortingOrder;
+		
+		[ShowIf("controlSortingOrder")]
+		public List<SpriteRenderer> spriteRenderers = new List<SpriteRenderer>();
+
+		public int sortingOrder;
+		
+		// Use this for initialization
+		void Start()
+		{
+			
+		}
+
+		void Update()
+		{
+			if (!pathway) return;
+			transform.position = pathway.Position(progress) + (Vector3)offset;
+
+			if (controlSortingOrder)
+			{
+				foreach (var s in spriteRenderers)
+				{
+					sortingOrder = pathway.SortingOrder(progress);
+					s.sortingLayerName = pathway.SortingLayer(progress);
+					s.sortingOrder = sortingOrder;
+				}
+			}
+		}
+	}
+}
---
+++ Maze_Shooter/Assets/Scripts/Paths/PathNode.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace Paths
+{
+	[ExecuteInEditMode]
+	public class PathNode : MonoBehaviour
+	{
+		[InfoBox("No pathway could be found in parents.", InfoMessageType.Warning, "NoPathway")]
+		public Pathway pathway;
+
+		bool NoPathway => pathway == null;
+		SpriteRenderer _spriteRenderer;
+		
+		// Use this for initialization
+		void Start()
+		{
+			if (!pathway) GetPathway();
+			pathway?.RefreshPathNodes();
+			_spriteRenderer = GetComponent<SpriteRenderer>();
+			
+			if (Application.isPlaying && _spriteRenderer) 
+				_spriteRenderer.enabled = false;
+		}
+
+		[Button]
+		public void GetSpriteRenderer()
+		{
+			_spriteRenderer = GetComponent<SpriteRenderer>();
+		}
+
+		[Button]
+		void GetPathway()
+		{
+			pathway = GetComponentInParent<Pathway>();
+		}
+
+		public string SortingLayer()
+		{
+			return _spriteRenderer == null ? "Default" : _spriteRenderer.sortingLayerName;
+		}
+
+		public int SortingOrder()
+		{
+			return _spriteRenderer == null ? 0 : _spriteRenderer.sortingOrder;
+		}
+	}
+}
