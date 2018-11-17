b86459ce314024a3b63614a7ad352aa21d87bb9c
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-11-17 15:50:00 -0600

    Add stage crystal
    Stage crystal is the world-map representation of a stage

---
+++ Maze_Shooter/Assets/Scripts/StageCrystal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageCrystal : MonoBehaviour
+{
+	public Stage stage;
+
+	Animator _animator;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
