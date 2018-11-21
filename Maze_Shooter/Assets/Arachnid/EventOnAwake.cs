e835a50aff012ba60fe59a3960d08f2bbf25d9fe
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-11-20 20:14:51 -0600

    Add eventOnAwake component

---
+++ Maze_Shooter/Assets/Arachnid/EventOnAwake.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Arachnid;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventOnAwake : MonoBehaviour
+{
+	public UnityEvent eventToInvoke;
+	public List<GameEvent> gameEvents = new List<GameEvent>();
+	
+	void Awake()
+	{
+		eventToInvoke.Invoke();
+		foreach (var e in gameEvents) e.Raise();
+	}
+}
