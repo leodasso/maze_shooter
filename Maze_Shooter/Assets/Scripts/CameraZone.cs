e2bf0ccae7214b987f0c422b7871171f16340474
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-11-21 21:40:37 -0600

    Add camera zone functionality, and some camera prefabs

--- Maze_Shooter/Assets/Scripts/CameraZone.cs
+++ Maze_Shooter/Assets/Scripts/CameraZone.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Arachnid;
 using Cinemachine;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class CameraZone : MonoBehaviour
@@ -9,12 +10,14 @@ public class CameraZone : MonoBehaviour
 	public CinemachineVirtualCamera vCam;
 	[Tooltip("An object that belongs to any of these collections will trigger this")]
 	public List<Collection> triggeringCollections;
-	
-	// Use this for initialization
-	void Start () 
-	{
-	}
 
+	[ToggleLeft]
+	public bool adjustPriority;
+
+	[ShowIf("adjustPriority")]
+	public int priorityDelta = 1;
+	
+	
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		CollectionElement element = other.GetComponent<CollectionElement>();
@@ -24,10 +27,25 @@ public class CameraZone : MonoBehaviour
 		EnterAction(other);
 	}
 
+	void OnTriggerExit2D(Collider2D other)
+	{
+		CollectionElement element = other.GetComponent<CollectionElement>();
+		if (!element) return;
+		
+		if (!triggeringCollections.Contains(element.collection)) return;
+		ExitAction(other);
+	}
+
 
 	void EnterAction(Collider2D other)
 	{
 		vCam.Follow = other.transform;
+		if (adjustPriority) vCam.Priority += priorityDelta;
+	}
+
+	void ExitAction(Collider2D other)
+	{
+		if (adjustPriority) vCam.Priority -= priorityDelta;
 	}
 
 }
