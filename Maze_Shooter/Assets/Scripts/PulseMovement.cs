439fb4d40214e43c67ebae5f4deb2f573a09be11
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-11-23 22:59:16 -0600

    Add new art, update dark orb with new art

--- Maze_Shooter/Assets/Scripts/PulseMovement.cs
+++ Maze_Shooter/Assets/Scripts/PulseMovement.cs
@@ -8,19 +8,27 @@ using UnityEngine.Events;
 [RequireComponent(typeof(TargetFinder))]
 public class PulseMovement : MonoBehaviour
 {
+	[TabGroup("main")]
 	public FloatReference startDelay;
-	[Tooltip("A random amount of this value will be added to start delay")]
+	[Tooltip("A random amount of this value will be added to start delay"), TabGroup("main")]
 	public FloatReference randomStartDelay;
 	
-	[Tooltip("Multiplies the movement speed")]
+	[Tooltip("Multiplies the movement speed"), TabGroup("main")]
 	public FloatReference speedMultiplier;
 
+	[TabGroup("main")]
 	[Tooltip("Multiplies the rate at which the curve is evaluated. Something like 'doing the pulse more times per minte'")]
 	public float timeMultiplier = 1;
+	
+	[TabGroup("main")]
 	public CurveObject speedCurve;
 
-	[DrawWithUnity]
+	[TabGroup("Events")]
+	[DrawWithUnity, Tooltip("A pulse is a run through the speed curve. This event will be called at the beginning of each pulse.")]
 	public UnityEvent onPulse;
+	
+	[TabGroup("Events"), Tooltip("Use this to call an event anywhere along the pulse timeline.")]
+	public List<PulseEvent> pulseEvents = new List<PulseEvent>();
 	
 	float _totalSpeed;
 	TargetFinder _targetFinder;
@@ -57,7 +65,14 @@ public class PulseMovement : MonoBehaviour
 		{
 			onPulse.Invoke();
 			_curveTime = 0;
+			foreach (var e in pulseEvents)
+				e.Reset();
 		}
+
+		float normalizedProgress = _curveTime / speedCurve.curve.Duration();
+		foreach (var e in pulseEvents)
+			e.TryEvent(normalizedProgress);
+		
 		_totalSpeed = speedMultiplier.Value * speedCurve.ValueFor(_curveTime) * timeMultiplier;
 	}
 
@@ -73,5 +88,28 @@ public class PulseMovement : MonoBehaviour
 	public void IncreaseTimeMultiplier(float amt)
 	{
 		timeMultiplier += amt;
+	}
+	
+	[System.Serializable]
+	public class PulseEvent
+	{
+		[Range(0, 1), Tooltip("Normalized time (0 is beginning, 1 is end) representing when to call this event.")]
+		public float eventTriggerTime = .5f;
+		[DrawWithUnity]
+		public UnityEvent pulseEvent;
+		bool _triggered;
+		
+		public void TryEvent(float time)
+		{
+			if (_triggered) return;
+			if (time < eventTriggerTime) return;
+			_triggered = true;
+			pulseEvent.Invoke();
+		}
+
+		public void Reset()
+		{
+			_triggered = false;
+		}
 	}
 }
