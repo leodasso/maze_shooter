04a944f9183f0d7aa20762cb747bb3b4a1a252e2
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-12-15 17:54:04 -0600

    Create intro cutscene, and enter/exit castle playmaker plays

---
+++ Maze_Shooter/Assets/Scripts/Playmaker/Tilemap/SetTilemapColor.cs
@@ -0,0 +1,36 @@
+﻿using HutongGames.PlayMaker;
+using HutongGames.PlayMaker.Actions;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SetTilemapColor : ComponentAction<Tilemap>
+{
+    [RequiredField]
+    public Tilemap tilemap;
+    [RequiredField]
+    public FsmColor color;
+    public bool everyFrame;
+
+    public override void Reset()
+    {
+        tilemap = null;
+    }
+
+    public override void OnEnter()
+    {
+        DoSetTilemapColor();
+        if (!everyFrame) Finish();
+    }
+
+    public override void OnUpdate()
+    {
+        if (everyFrame)
+            DoSetTilemapColor();
+    }
+
+    void DoSetTilemapColor()
+    {
+        if (!tilemap) return;
+        tilemap.color = color.Value;
+    }
+}
