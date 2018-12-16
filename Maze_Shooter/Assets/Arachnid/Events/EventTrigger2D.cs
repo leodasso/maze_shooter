2da64bcdffaa00b8fe0e9bb46f04c087b7626651
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-12-16 00:15:10 -0600

    Create a filtered trigger base class, extend event trigger from it

--- Maze_Shooter/Assets/Arachnid/Events/EventTrigger2D.cs
+++ Maze_Shooter/Assets/Arachnid/Events/EventTrigger2D.cs
@@ -7,48 +7,15 @@ using UnityEngine.Events;
 namespace Arachnid {
 
     [RequireComponent(typeof(Collider2D))]
-    public class EventTrigger2D : MonoBehaviour
+    [TypeInfoBox("When the attached 2D Collider is triggered, will Invoke the events contained below.")]
+    public class EventTrigger2D : FilteredTrigger2D
     {
         [AssetsOnly]
         public List<GameEvent> events = new List<GameEvent>();
-
         public UnityEvent uEvent;
 
-        [ToggleLeft, Tooltip("Will only trigger once.")]
-        public bool oneOff;
-
-        [ToggleLeft, Tooltip("Only allow triggers from objects of a particular collection")]
-        public bool filterTriggers = true;
-
-        [ShowIf("filterTriggers"), Tooltip("Any object in one of these collections can trigger this."), AssetsOnly]
-        public List<Collection> triggerers = new List<Collection>();
-
-        bool _triggered;
-
-
-        void OnTriggerEnter2D(Collider2D other)
+        protected override void OnTriggered(Collider2D triggerer)
         {
-            if (!filterTriggers)
-            {
-                Trigger();
-                return;
-            }
-
-            foreach (var c in triggerers)
-            {
-                if (c.ContainsGameObject(other.gameObject))
-                {
-                    Trigger();
-                    return;
-                }
-            }
-        }
-
-        void Trigger()
-        {
-            if (_triggered && oneOff) return;
-            
-            _triggered = true;
             uEvent.Invoke();
             foreach (var e in events) e.Raise();
         }
---
+++ Maze_Shooter/Assets/Arachnid/FilteredTrigger2D.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace Arachnid
+{
+	[RequireComponent(typeof(Collider2D))]
+	public class FilteredTrigger2D : MonoBehaviour
+	{
+
+		[ToggleLeft, Tooltip("Will only trigger once.")]
+		public bool oneOff;
+
+		[ToggleLeft, Tooltip("Only allow triggers from objects of a particular collection")]
+		public bool filterTriggers = true;
+
+		[ShowIf("filterTriggers"), Tooltip("Any object in one of these collections can trigger this."), AssetsOnly]
+		public List<Collection> triggerers = new List<Collection>();
+
+		bool _triggered;
+
+
+		void OnTriggerEnter2D(Collider2D other)
+		{
+			if (!filterTriggers)
+			{
+				Trigger(other);
+				return;
+			}
+
+			foreach (var c in triggerers)
+			{
+				if (c.ContainsGameObject(other.gameObject))
+				{
+					Trigger(other);
+					return;
+				}
+			}
+		}
+
+		void Trigger(Collider2D other)
+		{
+			if (_triggered && oneOff) return;
+			_triggered = true;
+			OnTriggered(other);
+		}
+
+		protected virtual void OnTriggered(Collider2D triggerer)
+		{
+		}
+	}
+}
