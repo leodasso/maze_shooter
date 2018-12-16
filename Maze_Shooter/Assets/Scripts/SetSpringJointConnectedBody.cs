17886fe400aff3c95425869e0944acec807fb3f3
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-12-16 00:14:47 -0600

    Create 'chain anchor'
    Chain anchor thing connectts to the player with a chain and pulls them towards the anchor!

---
+++ Maze_Shooter/Assets/Scripts/SetSpringJointConnectedBody.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Arachnid;
+using UnityEngine;
+
+public class SetSpringJointConnectedBody : FilteredTrigger2D
+{
+    public SpringJoint2D springJoint2D;
+
+    protected override void OnTriggered(Collider2D triggerer)
+    {
+        Rigidbody2D rb = triggerer.GetComponent<Rigidbody2D>();
+        if (!rb) return;
+        springJoint2D.connectedBody = rb;
+    }
+}
---
+++ Maze_Shooter/Assets/Scripts/SpringJoint2DLineRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpringJoint2D)), RequireComponent(typeof(LineRenderer))]
+public class SpringJoint2DLineRenderer : MonoBehaviour
+{
+
+	SpringJoint2D _springJoint2D;
+	LineRenderer _lineRenderer;
+	
+	Vector3[] _positions = new Vector3[2];
+
+	// Use this for initialization
+	void Awake ()
+	{
+		_springJoint2D = GetComponent<SpringJoint2D>();
+		_lineRenderer = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!_springJoint2D) return;
+		if (!_springJoint2D.connectedBody) return;
+		_positions[0] = (Vector3)_springJoint2D.anchor + transform.position;
+		_positions[1] = _springJoint2D.connectedBody.transform.position;
+		_lineRenderer.SetPositions(_positions);
+	}
+}
