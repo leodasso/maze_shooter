e52f816df3cb27b9225299510610311d0b40a1d3
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2018-12-29 17:24:58 -0600

    Add watergun stuff

---
+++ Maze_Shooter/Assets/Scripts/Water.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Arachnid;
+using UnityEngine;
+
+public class Water : ContactBase
+{
+	public FloatReference wetness;
+
+	protected override void OnCollisionAction(Collision2D collision, Collider2D otherCol)
+	{
+		IWettable wettable = otherCol.GetComponent<IWettable>();
+		wettable?.AddWater(wetness.Value);
+	}
+
+	protected override void OnTriggerAction(Collider2D other)
+	{
+		IWettable wettable = other.GetComponent<IWettable>();
+		wettable?.AddWater(wetness.Value);
+	}
+}
