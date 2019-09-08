992ccfb85a1f5a314a8750a0ba32c4451a639f08
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2019-09-08 16:39:55 -0500

    Add virtual cam helper with misc functions for dealing with virtual cam
    The only function so far allows for setting the priority of the attached cam for a certain amount of time, and then returning to the previous priority.

---
+++ Maze_Shooter/Assets/VirtualCamHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using Cinemachine;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class VirtualCamHelper : MonoBehaviour
+{
+    [MinValue(0.1f), Tooltip("When the priority of the attached virtual camera is overridden, how long before it returns" +
+                             "to its previous value.")]
+    public float priorityOverrideDuration = 5;
+    CinemachineVirtualCamera _vCam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _vCam = GetComponent<CinemachineVirtualCamera>();
+        if (!_vCam)
+        {
+            Debug.LogWarning("No virtual cam is attached to the virtual cam helper!", gameObject);
+            enabled = false;
+        }
+    }
+
+    public void OverridePriority(int newPriority)
+    {
+        int originalPriority = _vCam.Priority;
+        _vCam.Priority = newPriority;
+        StartCoroutine(SetPriority(originalPriority, priorityOverrideDuration));
+    }
+    
+
+    IEnumerator SetPriority (int priority, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _vCam.Priority = priority;
+    }
+}
