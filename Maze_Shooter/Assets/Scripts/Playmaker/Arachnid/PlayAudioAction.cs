fdec34df2687eb14642069e26c8b7d97431a9308
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2019-09-13 23:38:42 -0500

    Add playmaker custom actions

---
+++ Maze_Shooter/Assets/Scripts/Playmaker/Arachnid/PlayAudioAction.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+
+    [ActionCategory(ActionCategory.Audio)]
+    public class PlayAudioAction : FsmStateAction
+    {
+        [RequiredField]
+        [CheckForComponent(typeof(AudioAction))]
+        public FsmOwnerDefault gameObject;
+        
+        AudioAction _audioAction;
+        
+        
+        public override void Reset()
+        {
+            base.Reset();
+            gameObject = null;
+            _audioAction = null;
+        }
+
+        public override void OnEnter()
+        {
+            // get the audio action component
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			
+            if (go==null)
+            {
+                Finish();
+                return;
+            }
+			
+            _audioAction = go.GetComponent<AudioAction>();
+			
+            if (_audioAction==null)
+            {
+                Finish();
+                return;
+            }
+            
+            _audioAction.Play();
+        }
+    }
+}
---
+++ Maze_Shooter/Assets/Scripts/Playmaker/Arachnid/SetMoverSpeed.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using HutongGames.PlayMaker;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+
+    public class SetMoverSpeed : FsmStateAction
+    {
+        [RequiredField] [CheckForComponent(typeof(SimpleMovement))]
+        public FsmOwnerDefault gameObject;
+
+        public FsmFloat speed;
+
+        [Tooltip("Repeat every frame. Useful if the variables are changing.")]
+        public bool everyFrame;
+
+        SimpleMovement _mover;
+
+        public override void Reset()
+        {
+            gameObject = null;
+            _mover = null;
+        }
+
+        public override void OnEnter()
+        {
+            // get the animator component
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+
+            if (go == null)
+            {
+                Finish();
+                return;
+            }
+
+            _mover = go.GetComponent<SimpleMovement>();
+
+            if (_mover == null)
+            {
+                Finish();
+                return;
+            }
+
+            _mover.speedMultiplier = speed.Value;
+            if (!everyFrame) Finish();
+        }
+
+        public override void OnUpdate()
+        {
+            _mover.speedMultiplier = speed.Value;
+        }
+    }
+}
