225e0bd0fe2c5d220a9684cc3f250b4b87dd75e4
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2019-09-20 22:49:01 -0500

    Add a percentage mode to health event

--- Maze_Shooter/Assets/Scripts/Health and Damage/HealthEvent.cs
+++ Maze_Shooter/Assets/Scripts/Health and Damage/HealthEvent.cs
@@ -4,24 +4,40 @@ using UnityEngine;
 using Sirenix.OdinInspector;
 using UnityEngine.Events;
 
+public enum HealthEventMode
+{
+    hitPoints = 0,
+    percentage = 1,
+}
+
 [RequireComponent(typeof(Health))]
 public class HealthEvent : MonoBehaviour
 {
     [ReadOnly]
     public Health health;
 
-    [OnValueChanged("ClampHitPoints")]
+    [Tooltip("Is this event based on remaining hit points or percentage of health?")]
+    public HealthEventMode mode = HealthEventMode.hitPoints;
+
+    [OnValueChanged("ClampHitPoints"), ShowIf("modeIsHitPoints")]
     public int hitPointsOfEvent;
+
+    [Range(0, 1), HideIf("modeIsHitPoints"), OnValueChanged("UpdateHpOfEvent")]
+    public float percentageOfEvent = .5f;
 
     [DrawWithUnity]
     public UnityEvent healthEvent;
 
     bool _invoked;
 
+    public bool modeIsHitPoints => mode == HealthEventMode.hitPoints;
+
     void Start()
     {
         FindHealthComponent();
         health.onDamaged += CheckEvent;
+        
+        UpdateHpOfEvent();
     }
 
     void FindHealthComponent()
@@ -29,6 +45,15 @@ public class HealthEvent : MonoBehaviour
         health = GetComponent<Health>();
         if (!health) 
             Debug.LogWarning("Health Event component " + name + " requires a Health component on the same object in order to function.");
+    }
+
+    void UpdateHpOfEvent()
+    {
+        if (modeIsHitPoints) return;
+        if (!health) FindHealthComponent();
+        if (!health) return;
+
+        hitPointsOfEvent = Mathf.RoundToInt(health.hitPoints.Value * percentageOfEvent);
     }
 
     void ClampHitPoints()
