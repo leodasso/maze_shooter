250da36ce86aebf2d3088800070ba57f6d6f26f5
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2019-09-28 20:34:26 -0500

    Add fonts, create intro cutscene

---
+++ Maze_Shooter/Assets/Scripts/SpriteCopyColorOfTextMesh.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+using Sirenix.OdinInspector;
+
+[ExecuteAlways]
+public class SpriteCopyColorOfTextMesh : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public enum tmpType { Normal, UGUI}
+
+    public tmpType type = tmpType.Normal;
+
+    [ShowIf("isNormalTmp")]
+    public TextMeshPro textMesh;
+    [HideIf("isNormalTmp")]
+    public TextMeshProUGUI textMeshUgui;
+
+    Color _color;
+
+    [Button]
+    void GetSpriteRenderer()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    bool isNormalTmp => type == tmpType.Normal;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!spriteRenderer) return;
+        if (isNormalTmp && !textMesh) return;
+        if (!isNormalTmp && !textMeshUgui) return;
+
+        _color = isNormalTmp ? textMesh.color : textMeshUgui.color;
+
+        spriteRenderer.color = _color;
+    }
+}
