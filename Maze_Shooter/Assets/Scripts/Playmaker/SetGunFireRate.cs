a4856a790475c4d0c85e191dc0fb219ee48e725b
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2019-10-06 23:25:00 -0500

    Do more work on the turret tank buddy

---
+++ Maze_Shooter/Assets/Scripts/Playmaker/SetGunFireRate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using HutongGames.PlayMaker;
+using UnityEngine;
+
+public class SetGunFireRate : FsmStateAction
+{
+    public Gun gun;
+    [Range(0, 1), HutongGames.PlayMaker.Tooltip("Set the gun's fire rate intensity")]
+    public float newIntensity = .5f;
+
+    public bool resetOnExit;
+    float _previousValue = -99;
+
+    public override void Reset()
+    {
+        gun = null;
+    }
+    
+
+    public override void OnEnter()
+    {
+        if (gun)
+        {
+            _previousValue = gun.fireRateIntensity;
+            gun.fireRateIntensity = newIntensity;
+        }
+        Finish();
+    }
+    
+
+    public override void OnExit()
+    {
+        if (resetOnExit && _previousValue >= 0)
+            gun.fireRateIntensity = _previousValue;
+    }
+}
