c62e5393b2190ccb11a2b52b85c97ffdc27a503b
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2019-12-03 21:27:49 -0600

    Add some purgatory art, implement ghost animations

--- Maze_Shooter/Assets/Scripts/Movement/MovementAnimation.cs
+++ Maze_Shooter/Assets/Scripts/Movement/MovementAnimation.cs
@@ -7,19 +7,19 @@ public class MovementAnimation : MonoBehaviour, IControllable
     public Animator animator;
     [Tooltip("This doesn't affect movement! Just offsets the movement angle that gets sent to the animator.")]
     public float movementAngleOffset = 45;
-    Rigidbody2D _rigidbody2D;
+    Rigidbody _rigidbody;
     
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     public void ApplyLeftStickInput(Vector2 input)
     {
         if (!animator) return;
-        if (_rigidbody2D)
-            animator.SetFloat("speed", _rigidbody2D.velocity.magnitude);
+        if (_rigidbody)
+            animator.SetFloat("speed", _rigidbody.velocity.magnitude);
 			
         animator.SetFloat("facingAngle", Math.AngleFromVector2(input, movementAngleOffset));
     }
