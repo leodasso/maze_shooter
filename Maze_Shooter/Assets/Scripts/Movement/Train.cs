1e94421501bcf10b28affa5275316083d96124ce
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2019-12-04 21:17:13 -0600

    Add train functionality
    Training allows for multiple objects to follow each other like a train

---
+++ Maze_Shooter/Assets/Scripts/Movement/Train.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls a train of objects moving behind this game object. Trailing objects
+/// don't have to be children, but they must be referenced in the train element list,
+/// and they must have TrainElement components
+/// </summary>
+[ExecuteAlways]
+public class Train : MonoBehaviour
+{
+    public List<TrainElement> trainElements = new List<TrainElement>();
+    public float radius = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trainElements.Count < 1) return;
+        
+        // The first train element follows this
+        trainElements[0].Follow(transform, radius);
+        
+        for (int i = 1; i < trainElements.Count; i++)
+        {
+            var nextTrainElement = trainElements[i - 1];
+            trainElements[i].Follow(nextTrainElement.transform, nextTrainElement.radius);
+        }
+    }
+}
---
+++ Maze_Shooter/Assets/Scripts/Movement/TrainElement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A part of a train. A Train component can have many TrainElements following it,
+/// and they will organize into a nice little path, like, yknow, train cars.
+/// </summary>
+[ExecuteAlways]
+public class TrainElement : MonoBehaviour
+{
+    [Tooltip("Determines how much space is left before and after this element.")]
+    public float radius = 1;
+
+    // The ray cast from the leader toward this
+    Ray _leaderRay;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    public void Follow(Transform leader, float leaderRadius)
+    {
+        _leaderRay = new Ray(leader.position, transform.position - leader.position);
+        float followDist = radius + leaderRadius;
+        transform.position = _leaderRay.GetPoint(followDist);
+    }
+}
