8e2a8c1ff349ffe33b2c5fa0daae46e8fcca68f9
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2019-12-24 17:40:25 -0600

    Add haunt globs

--- Maze_Shooter/Assets/Scripts/Haunting/HauntGlob.cs
+++ Maze_Shooter/Assets/Scripts/Haunting/HauntGlob.cs
@@ -4,35 +4,20 @@ using UnityEngine.Events;
 
 public class HauntGlob : MonoBehaviour
 {
-    public float value = .1f;
-    [Tooltip("This acts as a scale multiplier. Total scale is a product of this and the value of the glob")]
-    public float scale = 1;
+    public int value = 1;
     public UnityEvent onGrabbed;
+
+    public bool _used;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        ScaleToValue();
-    }
 
-    public void SetValue(float newValue)
-    {
-        value = newValue;
-        ScaleToValue();
-    }
-
-    void ScaleToValue()
-    {
-        transform.localScale = Vector3.one * scale * value;
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter(Collider other)
     {
         Haunter haunter = other.GetComponent<Haunter>();
         if (!haunter) return;
         
+        if (_used) return;
+        _used = true;
         onGrabbed.Invoke();
         haunter.AddJuice(value);
-        Destroy(gameObject);
     }
 }
