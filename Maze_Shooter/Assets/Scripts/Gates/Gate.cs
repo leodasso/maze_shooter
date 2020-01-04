696663e6fcf7ef8f6d4e3476c65dcd45eac1624c
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2020-01-04 15:11:26 -0600

    WIP on gates

---
+++ Maze_Shooter/Assets/Scripts/Gates/Gate.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Arachnid;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class Gate : MonoBehaviour
+{
+    [ValidateInput("ValidateStars", "Required stars count must match the sum of stars in gate lights. Either adjust the count or " +
+                                    "add/remove gate lights.", ContinuousValidationCheck = true)]
+    public int requiredStars = 5;
+    public List<GateLight> gateLights = new List<GateLight>();
+    [AssetsOnly, Tooltip("The prop constellation that gets pulled out of the ghost and put into the gate lights")]
+    public GameObject propConstellationPrefab;
+    public Collection player;
+
+    [ShowInInspector, ReadOnly]
+    GameObject _player;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetPlayer()
+    {
+        _player = player.GetElement(0).gameObject;
+    }
+
+    bool ValidateStars(int inputStars)
+    {
+        int sum = 0;
+        foreach (GateLight gateLight in gateLights)
+        {
+            sum += gateLight.stars;
+        }
+
+        return sum == inputStars;
+    }
+
+    [Button]
+    void GetGateLights()
+    {
+        gateLights.Clear();
+        gateLights.AddRange(GetComponentsInChildren<GateLight>());
+    }
+}
---
+++ Maze_Shooter/Assets/Scripts/Gates/GateLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateLight : MonoBehaviour
+{
+    [Tooltip("Stars needed to activate this light. Should be kept to easy numbers like 1, 10, 50.")]
+    public int stars = 1;
+
+    [Tooltip("Is this light active? If all lights in a gate are active, the gate will activate.")]
+    public bool lightActive;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
