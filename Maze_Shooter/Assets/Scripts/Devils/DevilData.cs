60db620346dabb293c53d84373a1cd0138424992
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2020-01-08 21:02:06 -0600

    Organize devil scripts, add devil data

---
+++ Maze_Shooter/Assets/Scripts/Devils/DevilData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Ghost/Devil Data")]
+public class DevilData : SavedBool
+{
+    public string title = "Causer of Havoc";
+    protected override string Prefix => "devil_";
+    public GameObject prefab;
+}
