09cb554a4d56292062ea0e755ce6ec94a35833a6
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2020-01-11 18:08:19 -0600

    Add distance behavior to shadow

--- Maze_Shooter/Assets/Scripts/Effects/Shadow.cs
+++ Maze_Shooter/Assets/Scripts/Effects/Shadow.cs
@@ -6,14 +6,19 @@ public class Shadow : MonoBehaviour
     [AssetsOnly]
     public GameObject shadowPrefab;
 
+    public float shadowScale = 1;
+
     [Tooltip("Shadow is placed by raycasting down from this object. Which layers do you want to cast to?")]
     public LayerMask castingMask;
     GameObject shadowInstance;
     RaycastHit hit;
+    ShadowObject _shadowObject;
 
     void OnEnable()
     {
         shadowInstance = Instantiate(shadowPrefab, transform.position, shadowPrefab.transform.rotation);
+        shadowInstance.transform.localScale *= shadowScale;
+        _shadowObject = shadowInstance.GetComponent<ShadowObject>();
     }
     
     // Update is called once per frame
@@ -26,6 +31,8 @@ public class Shadow : MonoBehaviour
         }
         
         shadowInstance.transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        if (_shadowObject)
+            _shadowObject.SetDistance(transform.position.y);
     }
 
     void OnDisable()
