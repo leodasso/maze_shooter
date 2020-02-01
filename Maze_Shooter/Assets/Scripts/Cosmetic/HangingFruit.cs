2252f8279f22924e30a2d55f9d8fe883c4cf8ccd
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2020-02-01 13:54:35 -0600

    Add haunt tree, new gates, new glob

---
+++ Maze_Shooter/Assets/Scripts/Cosmetic/HangingFruit.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class HangingFruit : MonoBehaviour
+{
+    public Transform vine;
+    public Transform fruit;
+    public bool randomizeOnStart = true;
+    public float vineBaseLength = 1;
+    [HorizontalGroup("vine", Title = "Vine Length"), LabelText("min"), LabelWidth(30)]
+    public float minVineLength = 1;
+    [HorizontalGroup("vine"), LabelText("max"), LabelWidth(30)]
+    public float maxVineLength = 3;
+    [HorizontalGroup("fruit", Title = "Fruit Scale"), LabelText("min"), LabelWidth(30)]
+    public float minFruitScale = 1;
+    [HorizontalGroup("fruit"), LabelText("min"), LabelWidth(30)]
+    public float maxFruitScale = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(randomizeOnStart) Randomize();
+    }
+
+    [Button]
+    void Randomize()
+    {
+        float vineLength = Random.Range(minVineLength, maxVineLength);
+        vine.localScale = new Vector3(1, vineLength, 1);
+        fruit.localPosition = Vector3.down * vineLength * vineBaseLength;
+
+        float fruitScale = Random.Range(minFruitScale, maxFruitScale);
+        fruit.localScale = Vector3.one * fruitScale;
+    }
+}
---
+++ Maze_Shooter/Assets/Scripts/GrowthPatch.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Unity.Mathematics;
+using Math = Arachnid.Math;
+using Random = UnityEngine.Random;
+
+public class GrowthPatch : MonoBehaviour
+{
+    public int maxQty = 25;
+    public AnimationCurve spawnArea;
+    public List<GameObject> prefabs = new List<GameObject>();
+    public bool saveValue;
+    [ShowIf("saveValue")] 
+    public string uniqueId;
+    int qty;
+    List<GameObject> _instances = new List<GameObject>();
+    const string saveDataPrefix = "growthPatch_";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnMultiple(GetSavedQty());
+    }
+
+    int GetSavedQty()
+    {
+        return GameMaster.LoadFromCurrentFile(saveDataPrefix + uniqueId, maxQty, this);
+    }
+
+    public void Save()
+    {
+        int currentInstances = GetInstanceCount();
+        Debug.Log("Saved qty: " +  currentInstances);
+        GameMaster.SaveToCurrentFile(saveDataPrefix + uniqueId, currentInstances, this);
+    }
+
+    int GetInstanceCount()
+    {
+        int count = 0;
+        foreach (var instance in _instances)
+        {
+            if (!instance) continue;
+            if (!instance.activeInHierarchy) continue;
+            count++;
+        }
+
+        return count;
+    }
+
+    [Button]
+    void GenerateGuid()
+    {
+        Guid guid = Guid.NewGuid();
+        uniqueId = guid.ToString();
+    }
+
+    [ButtonGroup("spawn")]
+    void SpawnAll()
+    {
+        if (!Application.isPlaying)
+        {
+            Debug.Log("Enter play mode to test");
+            return;
+        }
+        SpawnMultiple(maxQty);
+    }
+
+    void SpawnMultiple(int qty)
+    {
+        Debug.Log("Spawning " + qty);
+        for (int i = 0; i < 5000; i++)
+        {
+            if (_instances.Count >= qty) return;
+            SpawnOne();
+        }
+    }
+
+    [ButtonGroup("spawn")]
+    void SpawnOne()
+    {
+        float radius = Random.Range(0, MaxRadius());
+        float diceRoll = Random.value;
+        if (diceRoll > spawnArea.Evaluate(radius)) return;
+
+        Vector3 pos = Random.onUnitSphere * radius;
+        Vector3 flatPos = new Vector3(pos.x, 0, pos.z);
+        var newInstance = Instantiate(Math.RandomElementOfList(prefabs), flatPos + transform.position, quaternion.identity, transform);
+        _instances.Add(newInstance);
+    }
+
+    float MaxRadius()
+    {
+        if (spawnArea == null) return 1;
+        if (spawnArea.keys.Length < 1) return 1;
+
+        var lastKey = spawnArea.keys[spawnArea.keys.Length - 1];
+        return lastKey.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
