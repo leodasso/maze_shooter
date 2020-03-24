30b9e6de5e1252ba7f7b8c3ecb5535c60ab6867c
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2020-03-24 17:24:23 -0500

    Add color profile system
    Color profiles help keep colors within a scene organized and easy to change

---
+++ Maze_Shooter/Assets/Scripts/Color/ColorElement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+/// <summary>
+/// This component goes on an object/prefab, and interfaces with the color manager of the current scene
+/// to make sure the colors of this object adhere to the scene's color profile.
+/// </summary>
+public class ColorElement : MonoBehaviour
+{
+    public ColorCategory colorCategory = ColorCategory.Primary;
+    public List<SpriteRenderer> spriteRenderers = new List<SpriteRenderer>();
+    public List<ColorGroup> colorGroups = new List<ColorGroup>();
+
+    [Button]
+    void AutoPopulate()
+    {
+        spriteRenderers.Clear();
+        spriteRenderers.AddRange(GetComponentsInChildren<SpriteRenderer>());
+        
+        colorGroups.Clear();
+        colorGroups.AddRange(GetComponentsInChildren<ColorGroup>());
+    }
+
+    
+    public void ApplyColorProfile(ColorProfile profile)
+    {
+        Color newColor = profile.GetColor(colorCategory);
+        
+        foreach (SpriteRenderer sr in spriteRenderers)
+            sr.color = newColor;
+
+        foreach (var colorGroup in colorGroups)
+        {
+            colorGroup.color = newColor;
+            colorGroup.SetColorFast(Color.white);
+        }
+    }
+}
---
+++ Maze_Shooter/Assets/Scripts/Color/ColorProfile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Ghost/Color Profile")]
+public class ColorProfile : ScriptableObject
+{
+    public Color primary = Color.white;
+    public Color secondary = Color.cyan;
+    public Color tertiary = Color.yellow;
+
+    public Color GetColor(ColorCategory category)
+    {
+        if (category == ColorCategory.Secondary) return secondary;
+        if (category == ColorCategory.Tertiary) return tertiary;
+        return primary;
+    }
+}
+
+public enum ColorCategory
+{
+    Primary,
+    Secondary,
+    Tertiary,
+}
