fe52be099bf672af340f91be7566ba6c0307f288
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2020-11-08 21:00:58 +0900

    add basic level growth progression thingy
    so that merging can cause more parts of the level to grow. Stuff within these 'level volumes' can grow at a specified level number like 1, 2, 3, etc

---
+++ Maze_Shooter/Assets/Scripts/Progress/LevelObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+
+public class LevelObject : MonoBehaviour
+{
+	[MinValue(0)]
+	[Tooltip("The level at which this object awakens. For always awake, leave at 0")]
+	public int minLevel = 0;
+
+	[ReadOnly]
+	public bool awake;
+
+	public List<LevelVolume> volumes = new List<LevelVolume>();
+
+	public UnityEvent onAwaken;
+	public UnityEvent onSleep;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	public void AddVolume(LevelVolume volume) {
+		if (volumes.Contains(volume)) return;
+		volumes.Add(volume);
+
+		if (awake) return;
+		int level = volumes.Count;
+		if (level >= minLevel) {
+			awake = true;
+			onAwaken.Invoke();
+		}
+	}
+
+	public void RemoveVolume(LevelVolume volume) {
+		volumes.Remove(volume);
+
+		if (!awake) return;
+		int level = volumes.Count;
+		if (level < minLevel) {
+			awake = false;
+			onSleep.Invoke();
+		}
+	}
+}
---
+++ Maze_Shooter/Assets/Scripts/Progress/LevelVolume.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelVolume : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	void OnTriggerEnter(Collider other) {
+
+		LevelObject levelObject = other.GetComponent<LevelObject>();
+		if (!levelObject) return;
+		levelObject.AddVolume(this);
+
+	}
+
+	void OnTriggerExit(Collider other) {
+		LevelObject levelObject = other.GetComponent<LevelObject>();
+		if (!levelObject) return;
+		levelObject.RemoveVolume(this);
+	}
+}
