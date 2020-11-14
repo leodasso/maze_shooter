3c41d6f0b0cbaa7d476019c847669cc34879935e
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2020-11-14 20:00:59 +0900

    more progress on the beginning scene

---
+++ Maze_Shooter/Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class SpriteAnimator : MonoBehaviour
+{
+	[Range(0, 1)]
+	public float progress = 0;
+    public SpriteRenderer spriteRenderer;
+    public List<Sprite> sprites = new List<Sprite>();
+    
+	public void Update() 
+	{
+		if (!spriteRenderer) return;
+		if (sprites.Count < 1) return;
+        int spriteIndex = Mathf.FloorToInt(progress * sprites.Count);
+        if (spriteIndex >= sprites.Count) 
+            spriteIndex = sprites.Count - 1;
+        spriteRenderer.sprite = sprites[spriteIndex];
+	}
+
+}
