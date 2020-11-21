f0e1d1fde5ee5cbaf0c7a8b82a6c440168329a96
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2020-11-21 19:55:01 +0900

    refactor ghost animation
    it's way way cooler and easier now

---
+++ Maze_Shooter/Assets/Scripts/Animation/GhostAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostAnimator : MonoBehaviour
+{
+	public SpriteAnimationPlayer animationPlayer;
+	public new Rigidbody rigidbody;
+
+	public AnimationCurve speedToFramerate = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+	public SpriteAnimation idle;
+	public SpriteAnimation run;
+	public float idleSpeed = .25f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		animationPlayer.speedMultiplier = speedToFramerate.Evaluate(rigidbody.velocity.magnitude);
+
+        animationPlayer.spriteAnimation =
+            rigidbody.velocity.magnitude > idleSpeed ? run : idle;
+    }
+}
