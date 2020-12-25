05309abf17b7bad4fd3e4770da5594df9ee799a0
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2020-12-25 23:18:43 +0900

    gate now saves and loads properly

---
+++ Maze_Shooter/Assets/Scripts/Playmaker/SaveBool.cs
@@ -0,0 +1,20 @@
+﻿using HutongGames.PlayMaker;
+
+public class SaveBool : FsmStateAction
+{
+	public SavedBool savedBool;
+	public bool newSavedValue;
+
+	public override void Reset()
+	{
+		savedBool = null;
+	}
+
+	public override void OnEnter()
+    {
+		if (savedBool) {
+			savedBool.Save(newSavedValue);
+		}
+		Finish();
+    }
+}
---
+++ Maze_Shooter/Assets/Scripts/Save/CheckSavedBool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CheckSavedBool : MonoBehaviour
+{
+	public SavedBool savedBool;
+	public bool checkOnStart = true;
+
+	public UnityEvent isTrue;
+	public UnityEvent isFalse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (checkOnStart) Check();
+    }
+
+	public void Check() 
+	{
+		if (savedBool.GetValue()) isTrue.Invoke();
+		else isFalse.Invoke();
+	}
+}
