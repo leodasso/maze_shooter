78f22fae2aad156c663856e4da107025edaccbdb
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2021-01-01 22:54:03 +0900

    polish galaxy

---
+++ Maze_Shooter/Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+	public Space space;
+	public Vector3 rotationSpeed;
+	public bool unscaledTime;
+	float deltaTime => unscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
+
+    void Update()
+    {
+        transform.Rotate(rotationSpeed * deltaTime, space);
+    }
+}
