a8f206a30d0ed266b90e132dd53ea02db49bc388
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2021-01-02 21:20:10 +0900

    crescent early prototype

---
+++ Maze_Shooter/Assets/Scripts/Crescents/Crescent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Crescent : MonoBehaviour
+{
+	public Orbiter myOrbiter;
+	public OrbiterGroup orbiterGroup;
+	public UnityEvent onCollected;
+
+	public void Collect() {
+		onCollected.Invoke();
+		if (orbiterGroup) orbiterGroup.AddOrbiter(myOrbiter);
+	}
+}
