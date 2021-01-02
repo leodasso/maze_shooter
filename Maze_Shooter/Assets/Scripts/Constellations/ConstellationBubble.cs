38af4a576a1f58d4b0b355e7e6f85d1bd09513d2
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2021-01-02 23:04:03 +0900

    add constellation bubble

---
+++ Maze_Shooter/Assets/Scripts/Constellations/ConstellationBubble.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+
+public class ConstellationBubble : MonoBehaviour
+{
+	public UnityEvent onUnlock;
+	public GameObject constellation;
+
+	[SerializeField]
+	float gravity = -10;
+	float ySpeed = 0;
+	bool falling = false;
+
+	[Button]
+	public void Unlock() {
+		onUnlock.Invoke();
+	}
+
+	public void StartFalling() {
+		falling = true;
+	}
+
+	public void StopFalling() {
+		falling = false;
+	}
+
+	void Update() {
+		if (falling) {
+			ySpeed += Time.unscaledDeltaTime * gravity;
+			transform.Translate(Vector3.up * ySpeed);
+		}else {
+			ySpeed = 0;
+		}
+	}
+
+	void OnDisable() {
+		constellation.transform.parent = null;
+	}
+}
