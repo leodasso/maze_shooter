c6c28d441ab6827240298d79f07e5c53f1f5341c
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2021-01-02 23:45:43 +0900

    animate bug legs

---
+++ Maze_Shooter/Assets/Scripts/Animation/PathLegDriver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathLegDriver : MonoBehaviour
+{
+	public float offset;
+	public float pathToLegMovement = 1;
+	public LegAnimator leg;
+	public PathFollower pathFollower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float pos = pathFollower.pathPosition + offset;
+		pos *= pathToLegMovement;
+		leg.progress = pos % 1;
+    }
+}
