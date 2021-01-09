b9e50430dd6e1dec0fcbd5be797d4630592f5a87
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2021-01-09 17:24:54 +0900

    add gui graphics, add support for HP gui

---
+++ Maze_Shooter/Assets/Scripts/UI/GuiHeart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+
+public class GuiHeart : MonoBehaviour
+{
+	public bool filled {
+		get {
+			return _filled;
+		}
+		set {
+			_filled = value;
+			image.sprite = filled ? filledSprite : emptySprite;
+		}
+	}
+
+	[Space, PreviewField]
+	public Sprite filledSprite;
+
+	[PreviewField]
+	public Sprite emptySprite;
+	public Image image;
+
+	bool _filled;
+
+	[ButtonGroup]
+	void Fill() {filled = true;}
+
+	[ButtonGroup]
+	void Empty() {filled = false;}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+}
---
+++ Maze_Shooter/Assets/Scripts/UI/HpGui.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Arachnid;
+
+public class HpGui : MonoBehaviour
+{
+	public IntValue currentHp;
+	public IntValue maxHp;
+	public List<GuiHeart> hearts = new List<GuiHeart>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Refresh();
+    }
+
+    public void Refresh() 
+	{
+		for (int i = 0; i < hearts.Count; i++) 
+		{
+			var heart = hearts[i];
+			heart.gameObject.SetActive(i < maxHp.Value);
+			if (heart.isActiveAndEnabled)
+				heart.filled = i < currentHp.Value;
+		}
+	}
+}
