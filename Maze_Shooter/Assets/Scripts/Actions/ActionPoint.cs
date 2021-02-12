e88de83692f840ca4c2889bbbd47bfb23b33aeac
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2021-02-13 00:20:50 +0900

    add gui functionality to action point

--- Maze_Shooter/Assets/Scripts/Actions/ActionPoint.cs
+++ Maze_Shooter/Assets/Scripts/Actions/ActionPoint.cs
@@ -6,6 +6,10 @@ using Sirenix.OdinInspector;
 
 public class ActionPoint : MonoBehaviour
 {
+
+	[Tooltip("Local position where the notification gui will appear")]
+	public Vector3 guiPosition = Vector3.up;
+
 	[Tooltip("If the player is within this distance, a notification icon will show")]
 	public float notifyDistance = 10;
 
@@ -20,10 +24,18 @@ public class ActionPoint : MonoBehaviour
 	[SerializeField]
 	UnityEvent onAction;
 
-	[SerializeField]
+	[ShowInInspector, ReadOnly]
 	ActionGui actionGuiInstance;
 
+	static GameObject actionIconsPrefab;
+
 	public static HashSet<ActionPoint> allActionPoints = new HashSet<ActionPoint>();
+
+	static void LoadActionIconsPrefab()
+	{
+		if (actionIconsPrefab) return;
+		actionIconsPrefab = Resources.Load("action icons") as GameObject;
+	}
 
 	void OnDrawGizmos() 
 	{
@@ -32,12 +44,24 @@ public class ActionPoint : MonoBehaviour
 
 		Gizmos.color = new Color(1, 1, 0, .8f);
 		GizmoExtensions.DrawCircle(transform.position, actionDistance);
+
+		Gizmos.DrawSphere(transform.position + guiPosition, .1f);
 	}
 
     void Awake()
     {
         allActionPoints.Add(this);
+		LoadActionIconsPrefab();
+		InstantiateGui();
     }
+
+	void InstantiateGui() 
+	{
+		GameObject newInstance = Instantiate(actionIconsPrefab, transform.position + guiPosition, Quaternion.identity);
+		newInstance.transform.parent = transform;
+
+		actionGuiInstance = newInstance.GetComponent<ActionGui>();
+	}
 
 	void OnDestroy()
 	{
