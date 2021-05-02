6d95428676e8b64861b30036120baea3d4f0a687
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2021-05-02 13:26:42 -0500

    fix bug in vcam zones
    vcam zones could possible add priority delta twice, which screws up everything.
    Just set it to only have 2 states:
    -priority when target in zone
    -init priority

--- Maze_Shooter/Assets/Scripts/Camera/CameraZone.cs
+++ Maze_Shooter/Assets/Scripts/Camera/CameraZone.cs
@@ -12,18 +12,31 @@ public class CameraZone : MonoBehaviour
 	[Tooltip("An object that belongs to any of these collections will trigger this")]
 	public List<Collection> triggeringCollections;
 
-	[ToggleLeft]
-	public bool adjustPriority;
-
-	[ToggleLeft, Tooltip("Set whatever triggers this as the follow target on the virtual camera.")]
+	[Space, ToggleLeft, Tooltip("Set whatever triggers this as the follow target on the virtual camera.")]
 	public bool overwriteFollowTarget = true;
 
-	[ShowIf("adjustPriority")]
-	public int priorityDelta = 1;
+	[ToggleLeft, Tooltip("Changes the priority of the vCam when a target is in this camera zone.")]
+	public bool adjustPriority;
+
+	[ShowIf("adjustPriority"), UnityEngine.Serialization.FormerlySerializedAs("priorityDelta")]
+	[Tooltip("The priority this vCam is at when there's a target in this camera zone.")]
+	public int activeCamPriority = 1;
+
+	[Space, ShowInInspector, ReadOnly]
+	bool targetInZone;
+
+	int initPriority;
+
+	void Awake() 
+	{
+		initPriority = vCam.Priority;
+	}
 	
 	
 	void OnTriggerEnter(Collider other)
 	{
+		if (targetInZone) return;
+
 		CollectionElement element = other.GetComponent<CollectionElement>();
 		if (!element) return;
 		
@@ -33,6 +46,8 @@ public class CameraZone : MonoBehaviour
 
 	void OnTriggerExit(Collider other)
 	{
+		if (!targetInZone) return;
+
 		CollectionElement element = other.GetComponent<CollectionElement>();
 		if (!element) return;
 		
@@ -43,13 +58,19 @@ public class CameraZone : MonoBehaviour
 
 	void EnterAction(Collider other)
 	{
-		if (overwriteFollowTarget) vCam.Follow = other.transform;
-		if (adjustPriority) vCam.Priority += priorityDelta;
+		targetInZone = true;
+		if (overwriteFollowTarget) 
+			vCam.Follow = other.transform;
+
+		if (adjustPriority) 
+			vCam.Priority = activeCamPriority;
 	}
 
 	void ExitAction(Collider other)
 	{
-		if (adjustPriority) vCam.Priority -= priorityDelta;
+		targetInZone = false;
+		if (adjustPriority) 
+			vCam.Priority = initPriority;
 	}
 
 }
