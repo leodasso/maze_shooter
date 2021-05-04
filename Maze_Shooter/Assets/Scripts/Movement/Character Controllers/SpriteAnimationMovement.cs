90498c0f065e2f7f74d0a5f52d5b6e32d3b6cf30
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2021-05-04 00:03:54 -0500

    add momentum to lightpost

--- Maze_Shooter/Assets/Scripts/Movement/Character Controllers/SpriteAnimationMovement.cs
+++ Maze_Shooter/Assets/Scripts/Movement/Character Controllers/SpriteAnimationMovement.cs
@@ -6,7 +6,12 @@ using Sirenix.OdinInspector;
 [ TypeInfoBox("Moves the playhead position of a sprite animation based on player input.") ]
 public class SpriteAnimationMovement : MonoBehaviour, IControllable
 {
-	public float speed = 1;
+	public float maxSpeed = 2;
+	public float acceleration = 1;
+	public float drag = 1;
+
+	float currentDrag;
+	float currentSpeed;
 
 	[Tooltip("The axis along which to accept input. For example, if this is (0, 1), then vertical joystick input " +
 	"will apply movement, but horizontal will not.")]
@@ -23,11 +28,23 @@ public class SpriteAnimationMovement : MonoBehaviour, IControllable
 		}
 	}
 
+	void Update() 
+	{
+		currentSpeed = Mathf.Lerp(currentSpeed, 0, Time.deltaTime * currentDrag);
+
+		spriteAnimator.progress += currentSpeed * Time.deltaTime;
+		spriteAnimator.progress = Mathf.Clamp01(spriteAnimator.progress);
+	}
+
 	public void ApplyLeftStickInput(Vector2 input) 
 	{
 		float inputAmount = Vector2.Dot(input, inputAxis);
-		spriteAnimator.progress += inputAmount * speed * Time.deltaTime;
-		spriteAnimator.progress = Mathf.Clamp01(spriteAnimator.progress);
+
+		currentSpeed += inputAmount * acceleration * Time.deltaTime;
+		currentSpeed = Mathf.Clamp(currentSpeed, -maxSpeed, maxSpeed);
+		
+		// only have drag if there's no player input
+		currentDrag = Mathf.Abs(inputAmount) < .25f ? drag : 0;
 	}
 
 	public void ApplyRightStickInput(Vector2 input) 
