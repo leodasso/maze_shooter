c6ba2c4ff6582969a0031fe6cf8e56b9380a3546
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2021-05-12 17:02:09 -0500

    create animation system for rubber band stretchy stuff

---
+++ Maze_Shooter/Assets/Scripts/Animation/RubberBandAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using ShootyGhost;
+
+public class RubberBandAnimator : MonoBehaviour
+{
+	public RubberBand rubberBand;
+	public SpriteAnimationPlayer spriteAnimationPlayer;
+
+	[Space]
+	[Tooltip("picks an animation from start of list to end based on how stretched the rubber band is.")]
+	public List<SpriteAnimation> animations = new List<SpriteAnimation>();
+
+    // Start is called before the first frame update
+    void Start()
+    {    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int animIndex = Mathf.FloorToInt(rubberBand.NormalizedRadius * animations.Count);
+		animIndex = Mathf.Clamp(animIndex, 0, animations.Count - 1);
+
+		spriteAnimationPlayer.spriteAnimation = animations[animIndex];
+    }
+}
