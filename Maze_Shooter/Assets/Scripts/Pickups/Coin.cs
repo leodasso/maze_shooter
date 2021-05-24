8afc84e14e0b918f5a8dee9f8b49a53f61874d9b
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2021-05-24 14:46:33 -0500

    add base class for pickups, move coin and money stuff into pickups folder

--- Maze_Shooter/Assets/Scripts/Pickups/Coin.cs
+++ Maze_Shooter/Assets/Scripts/Pickups/Coin.cs
@@ -1,35 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
+﻿using UnityEngine;
 
-public class Coin : MonoBehaviour
+public class Coin : Pickup
 {
-    public Vector3 spawnVelocity = new Vector3(0, 5, 0);
-    public float spawnVelocityRandomness = 3;
-
-    public new Rigidbody rigidbody;
-    public float destroyDelay = .5f;
-    public UnityEvent onGrabbed;
-	public bool jumpOnStart;
-    
+	[Space]
     public int value = 1;
-
-	public void Jump() 
-	{
-		rigidbody.isKinematic = false;
-        rigidbody.velocity = spawnVelocity + Random.insideUnitSphere * spawnVelocityRandomness;
-	}
-
-    // Start is called before the first frame update
-    void Start()
-    {  
-		if (jumpOnStart) Jump();
-	}
-
-    public void Grab()
-    {
-        onGrabbed.Invoke();
-        Destroy(gameObject, destroyDelay);
-    }
 }
---
+++ Maze_Shooter/Assets/Scripts/Pickups/Pickup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Pickup : MonoBehaviour
+{
+	public Vector3 spawnVelocity = new Vector3(0, 5, 0);
+    public float spawnVelocityRandomness = 3;
+
+	[SerializeField]
+	float destroyDelay = .5f;
+
+	[SerializeField]
+    protected UnityEvent onGrabbed;
+	public bool jumpOnStart;
+
+	[SerializeField]
+	protected new Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        if (jumpOnStart) Jump();
+    }
+
+	public void Jump() 
+	{
+		rigidbody.isKinematic = false;
+        rigidbody.velocity = spawnVelocity + Random.insideUnitSphere * spawnVelocityRandomness;
+	}
+
+	public void Grab()
+    {
+        onGrabbed.Invoke();
+        Destroy(gameObject, destroyDelay);
+    }
+}
