436096690e9f5837cb63ea1445bb8dfff252ed14
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2021-05-28 14:25:05 -0500

    complete island lantern prefab
    lantern lights a small island and can be toggled and has nice animations and stuff for being toggled

--- Maze_Shooter/Assets/Arachnid/FollowObject.cs
+++ Maze_Shooter/Assets/Arachnid/FollowObject.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 [ExecuteAlways]
 public class FollowObject : MonoBehaviour
 {
+	[HorizontalGroup, LabelWidth(20), ToggleLeft]
+	public bool x = true;
+
+	[HorizontalGroup, LabelWidth(20), ToggleLeft]
+	public bool y = true;
+
+	[HorizontalGroup, LabelWidth(20), ToggleLeft]
+	public bool z = true;
 
     public GameObject objectToFollow;
     public Vector3 offset;
@@ -13,6 +22,16 @@ public class FollowObject : MonoBehaviour
     void LateUpdate()
     {
         if (!objectToFollow) return;
-        transform.position = objectToFollow.transform.position + offset;
+
+		Vector3 followPos = objectToFollow.transform.position + offset;
+
+		Vector3 newPos = new Vector3 (
+			x ? followPos.x : transform.position.x,
+			y ? followPos.y : transform.position.y,
+			z ? followPos.z : transform.position.z
+		);
+
+
+        transform.position = newPos;
     }
 }
---
+++ Maze_Shooter/Assets/Scripts/Switch.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+
+public class Switch : MonoBehaviour
+{
+	[ShowInInspector, ToggleLeft, PropertyOrder(-200)]
+	public bool SwitchIsOn {
+		get {
+			return isOn;
+		}
+		set {
+			// ignore switching to same state
+			if (isOn == value) return;
+			isOn = value;
+
+			if (isOn) onTurnedOn.Invoke();
+			if (!isOn) onTurnedOff.Invoke();
+
+			onSwitched.Invoke(isOn);
+
+			if (playMaker) {
+				playMaker.SendEvent( isOn ? "turnOn" : "turnOff");
+			}
+		}
+	}
+
+	[Space, SerializeField, Tooltip("Sends events 'turnOn' and 'turnOff'")]
+	PlayMakerFSM playMaker;
+
+
+	[Space, SerializeField]
+	UnityEvent onTurnedOn;
+
+	[SerializeField]
+	UnityEvent onTurnedOff;
+
+	[SerializeField]
+	UnityEvent<bool> onSwitched;
+	
+
+	[SerializeField, HideInInspector]
+	bool isOn;
+
+	// below functions are like this for easy interface with playmaker
+	public void TurnOn()
+	{
+		SwitchIsOn = true;
+	}
+
+	public void turnOff()
+	{
+		SwitchIsOn = false;
+	}
+}
