bffaae85f75cf1ef3df9fe5b1276c4bdddb3ddaf
Leo-Alienware-Desktop <leo.dasso@gmail.com>
2021-06-30 23:21:39 -0500

    add bounds to star cursor

--- Maze_Shooter/Assets/Scripts/Constellations/StarCursor.cs
+++ Maze_Shooter/Assets/Scripts/Constellations/StarCursor.cs
@@ -6,9 +6,23 @@ using Arachnid;
 
 public class StarCursor : MonoBehaviour
 {
-	public FloatReference moveSpeed;
+	[SerializeField]
+	FloatReference moveSpeed;
+
+	[Tooltip("The size of the cursor's bounds, centered on the parent transform origin.")]
+	public Vector3 cursorBoundsSize;
+
 	Vector2 moveInput;
 	Rewired.Player _player;
+
+	void OnDrawGizmosSelected()
+	{
+		if (transform.parent)
+		{
+			Gizmos.color = Color.magenta;
+			Gizmos.DrawWireCube(transform.parent.position, cursorBoundsSize);
+		}
+	}
 
     void Start()
     {
@@ -19,5 +33,12 @@ public class StarCursor : MonoBehaviour
     {
 		moveInput = new Vector2(_player.GetAxis("moveX"), _player.GetAxis("moveY"));
 		transform.Translate(moveInput * Time.unscaledDeltaTime * moveSpeed.Value);
+
+		// clamp cursor to bounds
+		transform.localPosition = new Vector3(
+			Mathf.Clamp(transform.localPosition.x, -cursorBoundsSize.x/2, cursorBoundsSize.x/2),
+			Mathf.Clamp(transform.localPosition.y, -cursorBoundsSize.y/2, cursorBoundsSize.y/2),
+			Mathf.Clamp(transform.localPosition.z, -cursorBoundsSize.z/2, cursorBoundsSize.z/2)
+		);
     }
 }
